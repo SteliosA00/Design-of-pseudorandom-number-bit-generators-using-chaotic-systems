48d0f9b2e7bcc1632e1fd83e62581a4bdca6f731
SteliosA00 <152641997+SteliosA00@users.noreply.github.com>
2026-06-18 12:31:06 +0300

    Add files via upload

---
+++ Soboleva-Chaotic-PRBGs/Phase_diagrams/chebysev_phase_diagram.m
@@ -0,0 +1,30 @@
+%% Chebyshev map 
+clear
+x(1) = 0.5; 
+k = 2; 
+N = 10^5;
+
+for i = 2:N
+    x(i) = cos(k * acos(x(i-1)));
+end
+
+x = x(200:end); 
+
+figure
+plot(x(1:end-1), x(2:end), '.k')
+xlabel('x_{i}')
+ylabel('x_{i+1}')
+grid on
+box on
+set(gca,'fontsize',12)
+set(gca,'fontweight','bold')
+
+figure
+plot3(x(1:end-2), x(2:end-1), x(3:end), '.k')
+xlabel('x_{i}')
+ylabel('x_{i+1}')
+zlabel('x_{i+2}')
+grid on
+box on
+set(gca,'fontsize',12)
+set(gca,'fontweight','bold')
